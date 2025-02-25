a5f903b0eab9f9feb6371b611dc53516150960b2
Avinash77H <arakholiya5@gmail.com>
2025-02-25 14:57:14 +0530

    react icon library added

--- src/Component/PrimaryNavbar.jsx
+++ src/Component/PrimaryNavbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
+import { IoBagOutline } from "react-icons/io5";
+import { CiHeart } from "react-icons/ci";
+import { GoPerson } from "react-icons/go";
 
 function PrimaryNavbar() {
   return (
     <div className='border-1 '>
-      <nav>
+      <nav className='flex justify-between items-center'>
         {/* logo */}
         <div className=''>
           <img src={logo} alt="" />
@@ -19,6 +22,14 @@ function PrimaryNavbar() {
             <option value="Fashion">Fashion</option>
             <option value="Glocery">Glocery</option>
           </select>
+          <button className=''>Search</button>
+        </div>
+
+        {/* nav-icon */}
+        <div className='flex gap-2'>
+          <CiHeart/>
+          <IoBagOutline/>
+          <GoPerson/>
         </div>
       </nav>
     </div> 
