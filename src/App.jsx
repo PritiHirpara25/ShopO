71df1e6a6b1985b380196eb4a071bb0001161076
PritiHirpara25 <pritihirpara256@gmail.com>
2025-02-25 16:12:42 +0530

    Shop Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import PrimaryNavbar from './Component/PrimaryNavbar'
+// import Cards from './Helper/Cards'
+import Shop from './Pages/Shop'
 
 function App() {
 
   return (
     <>
         <PrimaryNavbar/>
+        <Shop/>
+        {/* <Cards/> */}
     </>
   )
 }
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import GameRemote from '../assets/GameRemote.jpg'
+// import GameRemote from '../assets/GameRemote.jpg'
+import Cards from '../Helper/Cards'
 
 const Shop = () => {
+
+  const card = [
+    { id: 1, image: GameRemote, title: "Xoggle aute et pariatur adipisicing nostrud et excepteur", discount: "$25.55", price: "$24.99" },
+    { id: 2, image: GameRemote, title: "Xoggle aute et pariatur adipisicing nostrud et excepteur", discount: "$25.55", price: "$24.99" },
+    { id: 3, image: GameRemote, title: "Xoggle aute et pariatur adipisicing nostrud et excepteur", discount: "$25.55", price: "$24.99" },
+    { id: 4, image: GameRemote, title: "Xoggle aute et pariatur adipisicing nostrud et excepteur", discount: "$25.55", price: "$24.99" },
+    { id: 5, image: GameRemote, title: "Xoggle aute et pariatur adipisicing nostrud et excepteur", discount: "$25.55", price: "$24.99" },
+    { id: 6, image: GameRemote, title: "Xoggle aute et pariatur adipisicing nostrud et excepteur", discount: "$25.55", price: "$24.99" },
+    { id: 7, image: GameRemote, title: "Xoggle aute et pariatur adipisicing nostrud et excepteur", discount: "$25.55", price: "$24.99" }
+  ]
+
   return (
-    <div>Shop</div>
+    <div>
+
+      <div className='grid grid-cols-4'>
+        {card.map((item) => (
+          <Cards key={item.id} product={item} />
+        )
+        )
+        }
+      </div>
+      {/* <img src={GameRemote} alt="" /> */}
+    </div>
   )
 }
 
