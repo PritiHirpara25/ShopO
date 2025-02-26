a07049151a955b6b35dfffe4ab69330daf8caa92
PritiHirpara25 <pritihirpara256@gmail.com>
2025-02-26 09:11:03 +0530

    Shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import PrimaryNavbar from './Component/PrimaryNavbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Cards from './Helper/Cards'
 import Shop from './Pages/Shop'
+import ProductDeatils from './Pages/ProductDeatils';
 
 function App() {
 
   return (
     <>
         <PrimaryNavbar/>
+        <Router>
+          <Routes>
+          {/* <Route path="/product/:id" element={<ProductDeatils />} /> */}
+          </Routes>
+        </Router>
         <Shop/>
         {/* <Cards/> */}
     </>
--- src/Pages/ProductDeatils.jsx
+++ src/Pages/ProductDeatils.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import GameRemote from '../assets/GameRemote.jpg'
+import { useParams } from 'react-router-dom'
 
 const ProductDeatils = () => {
+
+  const {id} = useParams;
+
+  const ProductData = [{ id: 1, image: GameRemote, title: "Xoggle aute et pariatur adipisicing nostrud et excepteur", discount: "$25.55", price: "$24.99" }]
+
   return (
-    <div>ProductDeatils</div>
+    <div>
+      {ProductData.map((item) => {
+        return(
+          <>
+            <h1>{item.title}</h1>
+            <img src={item.image} alt="" />
+            <p>{item.discount}</p>
+            <p>{item.price}</p>
+          </>
+        )
+      })}
+    </div>
   )
 }
 
