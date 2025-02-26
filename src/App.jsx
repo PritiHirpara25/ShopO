1416df23f087f56dac9268f8a5bfd60426e26db0
Avinash77H <arakholiya5@gmail.com>
2025-02-26 13:42:38 +0530

    'routingAdded'

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import './App.css'
-import PrimaryNavbar from './Component/PrimaryNavbar'
-import Sidebar from './Component/Sidebar'
+
 // import Cards from './Helper/Cards'
 import Shop from './Pages/Shop'
 import { useState } from 'react'
+import Navbar from './Component/Navbar'
 
 function App() {
   const[sidebar,setSidebar] = useState(false);
 
   return (
    <div >
-        <PrimaryNavbar sidebar={sidebar} setSidebar={setSidebar}/>
-        {/* <Sidebar /> */}
+        <Navbar sidebar={sidebar} setSidebar={setSidebar}/>
+       
         {/* <Shop/> */}
         {/* <Cards/> */}
     </div>
